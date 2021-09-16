0c503c3b249b8efcc8ac44138bb6bbd3de23ccbf
Diana Martine <Diana_martine@outlook.com>
2021-09-16 10:33:58 -0300

    aula 2 finalizada

---
+++ Aula 2 - entendendo variáveis e seus valores/script.js
@@ -0,0 +1,117 @@
+       /*  // tipos primitivos
+        // boolean
+        var boolean = false;
+        console.log(`A variável ${boolean} tem o tipo ${typeof(boolean)}`);
+
+        // number
+        var numero = 1;
+        console.log(`A variável ${numero} tem o tipo ${typeof(numero)}`);
+
+        // string
+        var nome = 'diana';
+        console.log(`A variável ${nome} tem o tipo ${typeof(nome)}`);
+
+        // function
+        var funcao = function(){};
+        console.log(`A variável ${funcao} tem o tipo ${typeof(funcao)}`);
+
+
+        // como declarar
+
+        var; // variável global ou local - valor inicial nulo
+
+        let; // variavel local de bloco - valor inicial nulo
+
+        const; // variável local de bloco - valor inicial obrigatório */
+
+        
+        // usando var global
+        /* var nome = 'diana';
+        var nome = 'Diana';
+
+        // usando var local
+        function nomeDaFuncao() {
+            var sobrenome = 'Martine';
+            console.log(sobrenome);
+        }
+
+        console.log(nome);
+        nomeDaFuncao(); */
+
+        // as propriedades de var se aplicam a let, mas por convenção usamos let no escopo local
+
+
+        /* // usando const
+        const constante = 'constante';
+        // valor não pode ser alterado; */
+
+
+
+        /* // atribuição
+        var atribuicao = 'diana';
+
+        // comparação
+        var comparacao = "0" == 0;
+        console.log(comparacao);
+
+        // comparação identica
+        var comparacaoIdentica = "0" === 0;
+        console.log(comparacaoIdentica); */
+
+
+       /*  // operadores aritméticos
+        // adição
+        var adicao = 1 + 1;
+        console.log(adicao);
+
+        //subtração
+        var subtracao = 2 - 1;
+        console.log(subtracao);
+
+        //multiplicação
+        var multiplicacao = 2 * 3;
+        console.log(multiplicacao);
+
+        //divisão real
+        var divisaoReal = 6 / 2;
+        console.log(divisaoReal);
+
+        //divisão inteira
+        var divisaoInteira = 5 % 2;
+        console.log(divisaoInteira);;
+
+        //potenciação
+        var potenciacao = 2**10;
+        console.log(potenciacao); */
+
+
+       /*  // operadores relacionais
+        // maior que
+        var maiorQue = 5 > 2;
+        console.log(maiorQue);
+        
+        // menor que
+        var menorQue = 5 < 2;
+        console.log(menorQue);
+        
+        // maior ou igual a
+        var maiorOuIgual = 5 >= 2;
+        console.log(maiorOuIgual);
+        
+        // menor ou igual a
+        var menorOuIgual = 5 <= 2;
+        console.log(menorOuIgual); */
+
+
+       /*  // operadores lógicos
+        // && - todos os valores devem ser true
+        var e = true && true;
+        console.log(e);
+
+        // || - somente um valor deve ser true
+        var ou = true || false;
+        console.log(ou);
+
+        // ! - inverte os valores
+        var nao = !true;
+        console.log(nao); */
