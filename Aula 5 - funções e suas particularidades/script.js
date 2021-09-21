5321ef1a570bec90254aff0694d6cb897d4028c4
Diana Martine <Diana_martine@outlook.com>
2021-09-21 10:39:22 -0300

    aula 5 finalizada

---
+++ Aula 5 - funções e suas particularidades/script.js
@@ -0,0 +1,20 @@
+// funções - functions
+
+// como declarar
+// deve ser declarada usando a palavra function seguida de "()" e "{}"
+/* function funcao() {
+    console.log('mensagem de uma função');
+}
+
+// chamando a função
+funcao(); */
+
+/* 
+// função com parâmetros
+// definindo parâmetros
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+// chamando a função atribuindo valor aos parâmetros
+mensagem('Tudo certo', 'jovem!'); */
