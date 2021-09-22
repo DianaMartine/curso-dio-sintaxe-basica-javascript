d8b12101bb65227e1774ecb449e69d3c582eee47
Diana Martine <Diana_martine@outlook.com>
2021-09-21 22:12:50 -0300

    aula 6 finalizada

---
+++ Aula 6 - aprofundando em funções/calculadora.js
@@ -0,0 +1,118 @@
+/* // criando uma calculadora
+function calculadora() {
+    
+    // definindo as operações e pegando valor de entrada do usuário
+    // usamos "\n" para fazer uma quebra de linha
+    const operacao = Number(prompt('Escolha uma opção:\n 1 - soma (+)\n 2 - subtração (-)\n 3 - multiplicação (*)\n 4 - divisão real (/)\n 5 - divisão inteira (%)\n 6 - potenciação (**)'));
+
+    // verificando se a operação é válida
+    if (!operacao) {
+        alert('erro - operação inválida');
+        calculadora();
+    } else {
+
+        // definindo variáveis
+        // todas as entradas devem ser do tipo number
+        let n1 = Number(prompt('Insira o primeiro valor:'));
+        let n2 = Number(prompt('Insira o segundo valor:'));
+        let resultado;
+
+        // verificando se as variáveis são válidas
+        if (!n1 || !n2) {
+            alert('erro - parâmetros inválidos');
+            calculadora();
+        } else {
+
+            // definindo as funções
+            function soma() {
+                resultado = n1 + n2;
+
+                // mostrando o resultado usando template strings
+                alert(`${n1} + ${n2} = ${resultado}`);
+                novaOperacao();
+            }
+
+            function subtracao() {
+                resultado = n1 - n2;
+                alert(`${n1} - ${n2} = ${resultado}`);
+                novaOperacao();
+            }
+
+            function multiplicacao() {
+                resultado = n1 * n2;
+                alert(`${n1} * ${n2} = ${resultado}`);
+                novaOperacao();
+            }
+
+            function divisaoReal() {
+                resultado = n1 / n2;
+                alert(`${n1} / ${n2} = ${resultado}`);
+                novaOperacao();
+            }
+
+            function divisaoInteira() {
+                resultado = n1 % n2;
+                alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`);
+                novaOperacao();
+            }
+
+            function potenciacao() {
+                resultado = n1 ** n2;
+                alert(`${n1} elevado à ${n2}ª potência é ${resultado}`);
+                novaOperacao();
+            }
+
+            // nova operação
+            function novaOperacao() {
+                let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
+
+                if (opcao == 1) {
+                    calculadora();
+                } else if (opcao == 2) {
+                    alert('Até mais!');
+                }
+            }
+        }
+    } */
+
+    
+    /* // escolhendo operação
+    // usand if && else
+    if (operacao == 1) {
+        soma();
+    } else if (operacao == 2) {
+        subtracao();
+    } else if (operacao == 3) {
+        multiplicacao();
+    } else if (operacao == 4) {
+        divisaoReal();
+    } else if (operacao == 5) {
+        divisaoInteira();
+    } else if (operacao == 6) {
+        potenciacao();
+    } */
+    
+    // usando switch case
+/*     switch (operacao) {
+        case 1:
+            soma();
+            break;
+        case 2:
+            subtracao();
+            break;
+        case 3:
+            multiplicacao();
+            break;
+        case 4:
+            divisaoReal();
+            break;
+        case 5:
+            divisaoInteira();
+            break;
+        case 6:
+            potenciacao();
+            break;
+    }
+}
+
+calculadora(); */
---
+++ Aula 6 - aprofundando em funções/tiposDeFuncao.js
@@ -0,0 +1,39 @@
+// tipos de função
+
+// função declarativa
+// deve ser declarada usando a palavra reservada function seguida do nome da funçao, parênteses "()" e chaves "{}"
+// deve ser nomeada obrigatioriamente
+
+/* function nomeDaFuncao() {
+    alert('Sou uma função declarativa!');
+}
+
+// devemos chamar a função para ver o resultado
+// chamando a função
+nomeDaFuncao(); */
+
+
+// expressão de função
+// são funções atribuídas à expressões
+// a nomeação é opcional
+
+// com nomeação
+/* var funcao = function funcao() {
+    alert('Sou uma expressão de função nomeada!');
+} */
+
+// sem nomeação
+/* var funcao = function() {
+    alert('Sou uma expressão de função não nomeada!')
+}
+*/
+
+// arrow Function
+// função de expressão de sintaxe curta
+// sempre anônima, não pode ser nomeada
+// deve ser declarada com parênteses "()", seguido de "=>" e depois chaves "{}"
+/* var funcao = () => {
+    alert('Sou uma função anônima!')
+};
+
+funcao(); */
